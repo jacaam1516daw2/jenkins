f4040c40aec959b8a7925017a912fc2df7ba6af0
jacaam1516daw2 <jacaam1516daw2@gmail.com>
2015-11-24 18:34:29 +0100

    Prova d'IC amb Jenkins commit 4

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -23,7 +23,7 @@ public class Jenkins {
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
             bw.write(" Modificació del projecte jenkins");bw.newLine();
-bw.write(" 2a Modificació del projecte jenkins);bw.newLine()
+//bw.write(" 2a Modificació del projecte jenkins);bw.newLine()
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
